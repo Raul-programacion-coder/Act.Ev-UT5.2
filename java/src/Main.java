b613f81102caf2cfb557ddff301023ec9eaf1e2f
Raul Correa <raulcorreainformatica@gmail.com>
2026-03-12 07:16:35 +0000

    Clase Nota , constructor , setter y getters

--- java/src/Main.java
+++ java/src/Main.java
@@ -4,4 +4,23 @@ public class Main {
 
 
     }
+
+
+}
+
+
+// ===================== CLASE NOTA =====================
+class Nota {
+    private String titulo;
+    private String contenido;
+
+    public Nota(String t, String c){ titulo=t; contenido=c; }
+    public String getTitulo(){ return titulo; }
+    public void setTitulo(String t){ titulo=t; }
+    public String getContenido(){ return contenido; }
+    public void setContenido(String c){ contenido=c; }
+
+
+    @Override
+    public String toString(){ return titulo; }
 }
